efbd669147ca6a5f0c0ee89a079722cbafb9a89d
DaanBakker <daan.bakker@infocaster.net>
2021-11-05 14:22:58 +0100

    Setup

---
+++ src/Infocaster.UmbracoAwesome.Icomoon/App_plugin/Icomoon/scripts/panel.controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    angular.module('umbraco')
+        .controller('infocaster.icomoonpanelcontroller', ['$scope', icomoonPanelController]);
+
+    function icomoonPanelController($scope) {
+
+        const $this = this;
+        $this.close = close;
+        $this.select = select;
+        $this.icons = [];
+
+        function close() {
+
+            $scope.model.close();
+        }
+
+        function select(icon) {
+
+            $scope.model.submit(icon);
+        }
+
+        function init() {
+
+            var iconElement = document.body.firstChild;
+            iconElement.querySelectorAll("symbol").forEach(function (s) {
+                $this.icons.push(s.id);
+            })
+        }
+
+        init();
+    }
+})()
